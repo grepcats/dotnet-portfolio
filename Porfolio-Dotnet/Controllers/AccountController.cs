645a73327bc3e0c88cfa960fd5505c889f0abc11
Kayla Ondracek <kayla.renee@gmail.com>
2018-05-04 09:05:10 -0700

    add ability to login and display user name in nav bar

--- Porfolio-Dotnet/Controllers/AccountController.cs
+++ Porfolio-Dotnet/Controllers/AccountController.cs
@@ -52,5 +52,24 @@ namespace Portfolio.Controllers
                 return View();
             }
         }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(model.Email, model.Password, isPersistent: true, lockoutOnFailure: false);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                return View();
+            }
+        }
     }
 }
